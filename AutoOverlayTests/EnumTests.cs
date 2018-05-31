30f691c37ebe11ca7427fb3563cc4f66930278b2
introspected <8684771+introspected@users.noreply.github.com>
2018-05-31 21:22:08 +0300

    Test package

---
+++ AutoOverlayTests/EnumTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoOverlay;
+using AvsFilterNet;
+using NUnit.Framework;
+
+namespace AutoOverlayTests
+{
+    [TestFixture]
+    public class EnumTests
+    {
+        [Test]
+        public void TestChromaSubsampling()
+        {
+            Assert.AreEqual(1, OverlayUtils.GetWidthSubsample(ColorSpaces.CS_YV24));
+            Assert.AreEqual(2, OverlayUtils.GetWidthSubsample(ColorSpaces.CS_YV12));
+            Assert.AreEqual(4, OverlayUtils.GetWidthSubsample(ColorSpaces.CS_YV411));
+        }
+    }
+}
